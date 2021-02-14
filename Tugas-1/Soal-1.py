7fd284b513baf2751a34ac00f0d8e75b44b996a0
aqshal-deandra <78999297+aqshal-deandra@users.noreply.github.com>
2021-02-14 15:16:30 +0700

    Create Soal-1.py

---
+++ Tugas-1/Soal-1.py
@@ -0,0 +1,12 @@
+# Aqshal Deandra Vandaru
+
+#------------ Nomor 1 -------------
+
+#Menerima 3 input dari user
+
+nama = input("Nama : ")
+umur = int(input("Umur : "))
+tinggi = float(input("Tinggi : "))
+
+#Mengeluarkan output
+print("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm.".format(nama, umur, tinggi))
